53f2f1b78fbfef222178b556197cdc712ec20d0f
ORUCALIYEV <orucaliyefff@gmail.com>
2022-10-19 02:28:17 +0400

    GlobalFilter added

--- controller/tourcontroller.js
+++ controller/tourcontroller.js
@@ -1,32 +1,64 @@
 const Tour = require("../model/tour");
+const GlobalFilter = require("../utils/GlobalFilter");
 
 exports.getAllTours = async (req, res) => {
-  const tours = await Tour.find;
+  //!MongoDb object
+  // let tours = Tour.find();
 
-  res.json({
-    success: true,
-    data: {
-      tours,
-    },
-  });
+  const tours = new GlobalFilter(Tour.find(), req.query);
+  tours.filter().sort().fields().paginate();
+
+  try {
+    const allData = await tours.query;
+
+    res.json({
+      success: true,
+      length: allData.length,
+      data: {
+        tours: allData,
+      },
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error,
+    });
+  }
 };
 
-exports.getOneTour = async (req, res) => {
-  const id = req.params.id;
+exports.getOneTour = async (req, res, next) => {
+  try {
+    const id = req.params.id;
 
-  const tour = Tour.find(id);
+    const tour = await Tour.find((t) => t.id == id);
 
-  if (!tour) return res.send({ success: false, message: "Invalid ID" });
+    if (!tour) return res.send({ success: false, message: "Invalid ID" });
 
-  res.json({ success: true, data: tour });
+    res.json({ success: true, data: { tour } });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error,
+    });
+  }
 };
 
-exports.createTour = async (req, res) => {
-  const newTour = await Tour.create([req.body]);
-  res.json({
-    success: true,
-    data: {
-      newTour,
-    },
-  });
+exports.createTour = async (req, res, next) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    console.log("here");
+
+    res.json({
+      success: true,
+      data: {
+        newTour,
+      },
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error,
+    });
+  }
 };
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -1,10 +1,15 @@
-const tourcontroller = require("../controller/tourcontroller");
 const router = require("express").Router();
+const tourController = require("../controller/tourController");
 
-router.get("/", tourcontroller.getAllTours);
+const top5Middleware = (req, res, next) => {
+  req.query.sort = "price -ratingsAverage";
+  req.query.limit = 5;
+  next();
+};
 
-// router.get("/:id", tourcontroller.getOneTour);
-
-router.post("/", tourcontroller.createTour);
+router.get("/", tourController.getAllTours);
+router.get("/top-5", top5Middleware, tourController.getAllTours);
+router.get("/:id", tourController.getOneTour);
+router.post("/", tourController.createTour);
 
 module.exports = router;
